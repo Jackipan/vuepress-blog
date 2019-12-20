100d93fd8b5c80e3089f3ac91e7dd205bcbe61c5
jackipan <2466366246@qq.com>
2019-12-20 16:14:11 +0800

    set base

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    title: 'jackipan',  // 设置网站标题
+    dest: './dist',  // 设置输出目录
+    base: '/vuepress-blog/', // 设置站点服务器目录
+    repo: 'https://github.com/jackipan/vuepress-blog' // 添加github链接
+}
